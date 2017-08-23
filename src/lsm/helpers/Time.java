bc30a1f652ba56441fb838941377f3a0df80eb49
Baizey <lassemoerk.mrk@gmail.com>
2017-08-23 23:46:43 +0200

    cleaned time up and standardized a couple of things

--- src/lsm/helpers/Time.java
+++ src/lsm/helpers/Time.java
@@ -10,34 +10,44 @@ import java.util.HashMap;
  * Time.write() will write the difference since last initialize call
  * Time.reset() will attempt to write time since last init and then re-initialize
  * A name can be given to take time on multiple things at once
- * Program is default name if none is given
+ * 'Program' is default name if none is given
  */
 
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class Time {
     private static final String defaultIndex = "Program";
-    private static final HashMap<String, Long> starts = new HashMap<>();
+    private static final HashMap<String, Long> starts = new HashMap<String, Long>(){{
+        put(defaultIndex, System.currentTimeMillis());
+    }};
 
-    public static void init () {
-        init(Time.defaultIndex);
-    }
-    public static void init (String name) {
-        Time.starts.put(name, System.currentTimeMillis());
+    public static void init() {
+        init(defaultIndex);
     }
 
-    public static void write () {
-        write(Time.defaultIndex);
+    public static void init(String name) {
+        starts.put(name, System.currentTimeMillis());
     }
-    public static void write (String name) {
-        long start = Time.starts.getOrDefault(name, 0L);
-        if (start == 0L) return;
+
+    public static void write() {
+        write(defaultIndex);
+    }
+
+    public static void write(String name) {
+        long start = starts.getOrDefault(name, 0L);
+
+        // Should probably throw an error message?
+        if (start == 0L)
+            return;
+
         long diff = System.currentTimeMillis() - start;
         Note.writenl(name + " took " + asSeconds(diff) + " seconds");
     }
 
-    public static void reset () {
-        reset(Time.defaultIndex);
+    public static void reset() {
+        reset(defaultIndex);
     }
-    public static void reset (String name) {
+
+    public static void reset(String name) {
         write(name);
         init(name);
     }
@@ -46,8 +56,7 @@ public class Time {
      * @param time, time in milliseconds
      * @returns String representing time in seconds
      */
-    private static String asSeconds (long time) {
-        return BigDecimal.valueOf(time).divide(Time.thousand, 3, RoundingMode.HALF_UP).toString();
+    private static String asSeconds(long time) {
+        return BigDecimal.valueOf(time).divide(BigDecimal.valueOf(1000L), 3, RoundingMode.HALF_UP).toString();
     }
-    private static final BigDecimal thousand = BigDecimal.valueOf(1000);
 }
